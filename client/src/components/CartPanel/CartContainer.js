821a09d6d27d4c60b288ddf7bebc393f4733720b
Codi <codisteinborn@gmail.com>
2018-03-26 14:32:50 -0500

    click handlers, storeconteiner created

--- client/src/components/CartPanel/CartContainer.js
+++ client/src/components/CartPanel/CartContainer.js
@@ -1,19 +1,18 @@
 import  React, {Component } from 'react';
-import { Row } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 import PayForm from './PayForm'
 
 class CartContainer extends Component {
    render(){
     return (
         <div>
+
+          <Col sm={12} mdOffset={6} md={6}>
              <PayForm />
+             </Col>
         </div>
         );
    }
   }
-
-// const CartContainer = props => (
-//     <PayForm />
-// );
 
 export default CartContainer;
