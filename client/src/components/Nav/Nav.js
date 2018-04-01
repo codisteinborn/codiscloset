43ae29d54919db27b95434576409b2ac89322d25
Codi <codisteinborn@gmail.com>
2018-04-01 14:40:38 -0500

    style

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -2,27 +2,14 @@ import React from "react";
 import { Col, Nav, NavItem, Navbar, MenuItem, NavDropdown } from 'react-bootstrap'
 import StoreContainer from './../../StoreContainer'
 import './Nav.css'
-import brand from './brand.png'
 
 
 const Navbr = props => (
-  // <Nav bsStyle="pills" style={{ height: 50, clear: 'both', backgroundColor: 'whitesmoke', position: 'relative' }}>
-  //   <NavItem className="navBrand" onClick={props.toggleHome}>
-  //     <img src={brand} alt='logo' className="logo"/>
-  //   </NavItem>
-  //   <MenuItem className="navRight" onClick={props.toggleLogin}>
-  //     LOG IN/SIGN UP
-  //     </MenuItem>
-  //   <NavItem className="navRight" onClick={props.toggleCart}>
-  //   <i className="fas fa-shopping-cart"></i>
-  //   </NavItem >
-  // </Nav>
 <div id="nav">
-{/* <img src={brand} alt='logo' className="logo" onClick={props.toggleHome}/> */}
 <div className="navBrand" onClick={props.toggleHome}>codi's closet</div>
 
-<div onClick={props.toggleCart} className=" navRight">cart</div>
-
+<div onClick={props.toggleCart} className=" navRight">CART</div>
+   {/* <i className="fas fa-shopping-cart"></i> */}
 <div className="navRight" onClick={props.toggleLogin}> LOG IN/SIGN UP</div>
 </div>
 
--- client/src/components/ProductPanel/ProductContainer.js
+++ client/src/components/ProductPanel/ProductContainer.js
@@ -10,14 +10,14 @@ import Jumbo from '../Jumbotron'
 const ProductContainer = props => (
         <div>
             <Jumbo />
-            <Row>
+            <div>
                 <Col xs={0} sm={0} md={2}>
                     <CategoryPanel xs={0} sm={0} md={2} catSelect={props.catSelect} catRemove={props.catRemove} />
                 </Col>
                 <Col xs={12} sm={12} md={10}>
                     {props.prodArr.map(elem => <ProdPrev category={elem.category} name={elem.name} price={elem.price} key={elem._id} image={elem.image} clicker={props.clicker} id={elem._id}/>)}
                 </Col>
-            </Row>
+            </div>
         </div> 
          
 );
