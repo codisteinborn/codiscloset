bdb512702f905e63f87cd4da1163da261ceaad8c
Codi <codisteinborn@gmail.com>
2018-04-02 16:03:36 -0500

    login and then welcome username

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProductContainer from './components/ProductPanel/ProductContainer';
 import CartContainer from './components/CartPanel/CartContainer';
-import LoginContainer from './components/LoginPanel/LoginContainer'
+// import LoginContainer from './LoginContainer'
 import StoreContainer from './StoreContainer'
 
 const App = () => (
@@ -11,7 +11,7 @@ const App = () => (
       <Switch>
       <Route exact path="/" component={StoreContainer} />
       <Route exact path="/cart" component={StoreContainer} />
-      <Route exact path="/login" component={LoginContainer} />
+      {/* <Route exact path="/login" component={LoginContainer} /> */}
       </Switch>
     </div>
   </Router>
