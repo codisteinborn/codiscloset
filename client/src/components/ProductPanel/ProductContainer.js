7bd7b850aed3e15758d3c101702dbc757103a1b1
Codi <codisteinborn@gmail.com>
2018-04-03 11:56:10 -0500

    basic payform validation

--- client/src/components/ProductPanel/ProductContainer.js
+++ client/src/components/ProductPanel/ProductContainer.js
@@ -11,9 +11,6 @@ const ProductContainer = props => (
         <div>
             <Jumbo />
             <div>
-                {/* <Col xs={0} sm={0} md={2}>
-                    <CategoryPanel xs={0} sm={0} md={2} catSelect={props.catSelect} catRemove={props.catRemove} />
-                </Col> */}
                 <Col xs={12} sm={12} md={12}>
                     {props.prodArr.map(elem => <ProdPrev category={elem.category} name={elem.name} price={elem.price} key={elem._id} image={elem.image} clicker={props.clicker} id={elem._id}/>)}
                 </Col>
